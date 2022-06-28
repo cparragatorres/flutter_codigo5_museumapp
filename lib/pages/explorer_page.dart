a2a1d56c3b6f99161e38d53b6e83e029fe687b30
Elvis Barrionuevo <ehbarrionuevo@gmail.com>
2022-06-27 22:50:49 -0500

    Responsive 3

--- lib/pages/explorer_page.dart
+++ lib/pages/explorer_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_codigo5_museumapp/helpers/data/data_dummy.dart';
+import 'package:flutter_codigo5_museumapp/ui/general/general_widget.dart';
 import 'package:flutter_codigo5_museumapp/ui/responsive/responsive.dart';
 
 class ExplorerPage extends StatelessWidget {
@@ -6,7 +8,6 @@ class ExplorerPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-
     print(ResponsiveUI.of(context).hp(10));
 
     ResponsiveUI _responsive = ResponsiveUI(context);
@@ -16,12 +17,69 @@ class ExplorerPage extends StatelessWidget {
     print(ResponsiveUI.pWidth(context, 0.5));
     print(ResponsiveUI.pHeight(context, 0.3));
 
-
     return SingleChildScrollView(
-      child: Column(
-        children: [
-
-        ],
+      child: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            SingleChildScrollView(
+              physics: const BouncingScrollPhysics(),
+              scrollDirection: Axis.horizontal,
+              child: Row(
+                children: imagesDummy
+                    .map(
+                      (e) => Container(
+                        height: 180,
+                        margin: const EdgeInsets.only(right: 12.0),
+                        width: ResponsiveUI.of(context).wp(85),
+                        decoration: BoxDecoration(
+                          color: Colors.red,
+                          borderRadius: BorderRadius.circular(12.0),
+                          image: DecorationImage(
+                            fit: BoxFit.cover,
+                            image: NetworkImage(e),
+                          ),
+                        ),
+                      ),
+                    )
+                    .toList(),
+              ),
+            ),
+            divider30,
+            Text(
+              "Categorías",
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 16.0,
+              ),
+            ),
+            divider20,
+            SingleChildScrollView(
+              physics: const BouncingScrollPhysics(),
+              scrollDirection: Axis.horizontal,
+              child: Row(
+                children: imagesDummy
+                    .map(
+                      (e) => Container(
+                        height: 180,
+                        margin: const EdgeInsets.only(right: 12.0),
+                        width: ResponsiveUI.of(context).wp(45),
+                        decoration: BoxDecoration(
+                          color: Colors.red,
+                          borderRadius: BorderRadius.circular(12.0),
+                          image: DecorationImage(
+                            fit: BoxFit.cover,
+                            image: NetworkImage(e),
+                          ),
+                        ),
+                      ),
+                    )
+                    .toList().reversed.toList(),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }

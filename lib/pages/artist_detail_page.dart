c5288b5a93097823a16a7289b55d04d438181dc3
Elvis Barrionuevo <ehbarrionuevo@gmail.com>
2022-06-28 21:55:30 -0500

    ArtistDetailPage 6

--- lib/pages/artist_detail_page.dart
+++ lib/pages/artist_detail_page.dart
@@ -4,7 +4,15 @@ import 'package:flutter_codigo5_museumapp/ui/general/general_widget.dart';
 import 'package:flutter_codigo5_museumapp/ui/responsive/responsive.dart';
 import 'package:flutter_codigo5_museumapp/ui/widgets/my_app_bar_widget.dart';
 
-class ArtistDetailPage extends StatelessWidget {
+class ArtistDetailPage extends StatefulWidget {
+  @override
+  State<ArtistDetailPage> createState() => _ArtistDetailPageState();
+}
+
+class _ArtistDetailPageState extends State<ArtistDetailPage> {
+
+  bool isExpanded = false;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -31,54 +39,86 @@ class ArtistDetailPage extends StatelessWidget {
                 ),
               ],
             ),
-            divider40,
-            Text(
-              "Vincent van Gogh",
-              style: TextStyle(
-                color: Colors.white,
-                fontSize: 28.0,
-              ),
-            ),
-            divider12,
-            Text(
-              "Mar 30 1853 - Jul 29 1890",
-              style: TextStyle(
-                color: Colors.white70,
-                fontSize: 14.0,
-              ),
-            ),
-            divider30,
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                IconButton(
-                  onPressed: () {},
-                  icon: Icon(
-                    Icons.favorite_border_outlined,
-                    color: Colors.white,
+            Padding(
+              padding: const EdgeInsets.all(14.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  divider40,
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        "Vincent van Gogh",
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 28.0,
+                        ),
+                      ),
+                    ],
                   ),
-                ),
-                dividerWidth12,
-                dividerWidth6,
-                IconButton(
-                  onPressed: () {},
-                  icon: Icon(
-                    Icons.share,
-                    color: Colors.white,
+                  divider12,
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        "Mar 30 1853 - Jul 29 1890",
+                        style: TextStyle(
+                          color: Colors.white70,
+                          fontSize: 14.0,
+                        ),
+                      ),
+                    ],
                   ),
-                )
-              ],
-            ),
-            divider30,
-            Text(
-              '''Nació el 30 de marzo de 1853. Hijo de un austero y humilde pastor protestante neerlandés llamado Theodorus y de su mujer Anna Cornelia, Vincent recibió el mismo nombre que le habían puesto a un hermano que nació muerto exactamente un año antes. El 1 de mayo de 1857 nació su hermano Theo y ambos tuvieron cuatro hermanos más: Cornelius Vincent, Elisabetha Huberta, Anna Cornelia y Wilhelmina Jacoba.
+                  divider30,
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      IconButton(
+                        onPressed: () {},
+                        icon: Icon(
+                          Icons.favorite_border_outlined,
+                          color: Colors.white,
+                        ),
+                      ),
+                      dividerWidth12,
+                      dividerWidth6,
+                      IconButton(
+                        onPressed: () {},
+                        icon: Icon(
+                          Icons.share,
+                          color: Colors.white,
+                        ),
+                      )
+                    ],
+                  ),
+                  divider30,
+                  Text(
+                    '''Nació el 30 de marzo de 1853. Hijo de un austero y humilde pastor protestante neerlandés llamado Theodorus y de su mujer Anna Cornelia, Vincent recibió el mismo nombre que le habían puesto a un hermano que nació muerto exactamente un año antes. El 1 de mayo de 1857 nació su hermano Theo y ambos tuvieron cuatro hermanos más: Cornelius Vincent, Elisabetha Huberta, Anna Cornelia y Wilhelmina Jacoba.
 Durante la infancia acudió a la escuela de manera discontinua e irregular, pues sus padres le enviaron a diferentes internados. El primero de ellos en Zevenbergen en 1864, donde estudió francés y alemán. Dos años después se matriculó en la escuela secundaria HBS Koning Willem II (Tilburg) viviendo con la familia Hannik en la calle Sint Annaplein 18-19 y permaneció allí hasta que dejó los estudios de manera definitiva a los quince años. Por entonces comenzó su afición por la pintura.
-                ''',
-              maxLines: 3,
-              overflow: TextOverflow.ellipsis,
-              style: TextStyle(
-                height: 1.4,
-                color: Colors.white,
+                  ''',
+                    maxLines: !isExpanded ? 3 : null,
+                    overflow: !isExpanded ? TextOverflow.ellipsis : TextOverflow.visible,
+                    style: TextStyle(
+                      height: 1.4,
+                      color: Colors.white,
+                    ),
+                  ),
+                  divider6,
+                  GestureDetector(
+                    onTap: (){
+                      isExpanded = !isExpanded;
+                      setState((){});
+                    },
+                    child: Text(
+                      !isExpanded ? "Más información" : "Menos información",
+                      style: TextStyle(
+                        color: Colors.blue,
+                      ),
+                    ),
+                  ),
+                ],
               ),
             ),
           ],

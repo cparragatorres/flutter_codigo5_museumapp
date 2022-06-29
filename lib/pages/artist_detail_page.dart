c2dd1bd01a33481663afcd02d85bcba86a343260
Elvis Barrionuevo <ehbarrionuevo@gmail.com>
2022-06-28 21:11:08 -0500

    ArtistDetailPage 1

---
+++ lib/pages/artist_detail_page.dart
@@ -0,0 +1,11 @@
+
+import 'package:flutter/material.dart';
+
+class ArtistDetailPage extends StatelessWidget {
+  const ArtistDetailPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
--- lib/ui/widgets/item_artist_grid_widget.dart
+++ lib/ui/widgets/item_artist_grid_widget.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_codigo5_museumapp/pages/artist_detail_page.dart';
 import 'package:flutter_codigo5_museumapp/ui/general/colors.dart';
 
 class ItemGridWidget extends StatelessWidget {
@@ -10,54 +11,59 @@ class ItemGridWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-          image: DecorationImage(
-        fit: BoxFit.cover,
-        image: NetworkImage(
-          image,
-        ),
-      )),
-      child: Stack(
-        children: [
-          Container(
-            decoration: BoxDecoration(
-              gradient: LinearGradient(
-                begin: Alignment.bottomCenter,
-                end: Alignment.center,
-                colors: [
-                  kBrandPrimaryColor.withOpacity(0.7),
-                  kBrandPrimaryColor.withOpacity(0.05),
+    return GestureDetector(
+      onTap: (){
+        Navigator.push(context, MaterialPageRoute(builder: (context)=> ArtistDetailPage()));
+      },
+      child: Container(
+        decoration: BoxDecoration(
+            image: DecorationImage(
+          fit: BoxFit.cover,
+          image: NetworkImage(
+            image,
+          ),
+        )),
+        child: Stack(
+          children: [
+            Container(
+              decoration: BoxDecoration(
+                gradient: LinearGradient(
+                  begin: Alignment.bottomCenter,
+                  end: Alignment.center,
+                  colors: [
+                    kBrandPrimaryColor.withOpacity(0.7),
+                    kBrandPrimaryColor.withOpacity(0.05),
+                  ],
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(12.0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.end,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    "Lorem ipsumsd",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.w600,
+                      fontSize: 13.0,
+                    ),
+                  ),
+                  Text(
+                    "234 items",
+                    style: TextStyle(
+                      fontSize: 12.0,
+                      color: Colors.white,
+                      fontWeight: FontWeight.w500,
+                    ),
+                  ),
                 ],
               ),
             ),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(12.0),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.end,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  "Lorem ipsumsd",
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontWeight: FontWeight.w600,
-                    fontSize: 13.0,
-                  ),
-                ),
-                Text(
-                  "234 items",
-                  style: TextStyle(
-                    fontSize: 12.0,
-                    color: Colors.white,
-                    fontWeight: FontWeight.w500,
-                  ),
-                ),
-              ],
-            ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }

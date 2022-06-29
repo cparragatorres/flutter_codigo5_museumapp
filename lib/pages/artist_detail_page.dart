c341e020b9caf402a19c3d087046cfb58564a32e
Elvis Barrionuevo <ehbarrionuevo@gmail.com>
2022-06-28 21:38:06 -0500

    ArtistDetailPage 3

--- lib/pages/artist_detail_page.dart
+++ lib/pages/artist_detail_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_codigo5_museumapp/ui/general/colors.dart';
+import 'package:flutter_codigo5_museumapp/ui/general/general_widget.dart';
 import 'package:flutter_codigo5_museumapp/ui/responsive/responsive.dart';
 import 'package:flutter_codigo5_museumapp/ui/widgets/my_app_bar_widget.dart';
 
@@ -30,9 +31,44 @@ class ArtistDetailPage extends StatelessWidget {
                 ),
               ],
             ),
-
-
-
+            divider40,
+            Text(
+              "Vincent van Gogh",
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 28.0,
+              ),
+            ),
+            divider12,
+            Text(
+              "Mar 30 1853 - Jul 29 1890",
+              style: TextStyle(
+                color: Colors.white70,
+                fontSize: 14.0,
+              ),
+            ),
+            divider30,
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                IconButton(
+                  onPressed: () {},
+                  icon: Icon(
+                    Icons.favorite_border_outlined,
+                    color: Colors.white,
+                  ),
+                ),
+                dividerWidth12,
+                dividerWidth6,
+                IconButton(
+                  onPressed: () {},
+                  icon: Icon(
+                    Icons.share,
+                    color: Colors.white,
+                  ),
+                )
+              ],
+            ),
           ],
         ),
       ),

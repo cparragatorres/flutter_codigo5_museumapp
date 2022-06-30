261629e05f7f62a00fe6a9e2de329014c3e74630
Elvis Barrionuevo <ehbarrionuevo@gmail.com>
2022-06-29 19:37:49 -0500

    Sevices 4

--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -1,9 +1,10 @@
+import 'dart:async';
+import 'dart:io';
 
 import 'package:logger/logger.dart';
 import 'package:http/http.dart' as http;
 
 class APIService {
-
   Logger logger = Logger();
 
   getMuseums() async {
@@ -14,8 +15,14 @@ class APIService {
       http.Response response = await http.get(_uri);
       print(response.statusCode);
       print(response.body);
-    } catch (error) {
-      logger.e(error);
+    } on TimeoutException catch (error) {
+      logger.w(error);
+    } on SocketException catch (error) {
+      logger.w(error);
+    } on FormatException catch (error) {
+      logger.w(error);
+    } on Error catch (error) {
+      print("ddddddddd $error");
     }
   }
 }
